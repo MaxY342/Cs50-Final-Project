77f0593cd475ca7df527e62300e2c5694b756798
YessirDaniel <rudolphiscool1000@gmail.com>
2024-07-12 18:18:09 -0400

    LocalMenuUpdate
    LocalMenu Update(Added logic to switch the scene when pressing local)

---
+++ Player v Player Game/Assets/Scripts/IntroScene/LocalActivityButton.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LocalActivityButton : MonoBehaviour
+{
+    public void LoadScene(string LocalMenu)
+    {
+        SceneManager.LoadScene("LocalMenu");
+    }
+}

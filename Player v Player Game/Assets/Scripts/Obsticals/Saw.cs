9c5c0c386dcef9455425776a0afb618d01560e37
Max <max.yang342@gmail.com>
2024-07-21 22:35:08 -0400

    reorganize and update saw scripts
    add logic for player collision with the saws and for when the saw obstacle will fall/move

---
+++ Player v Player Game/Assets/Scripts/Obsticals/Saw.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+namespace Player_v_Player_Game.Player
+{
+    public class Saw : MonoBehaviour
+    {
+        public float rotationSpeed = -180f; // degrees per second
+        private HealthManager healthManager;
+
+        void Start()
+        {
+            healthManager = FindObjectOfType<HealthManager>();
+        }
+
+        void Update()
+        {
+            SpinSaw();
+        }
+
+        private void SpinSaw()
+        {
+            transform.Rotate(Vector3.forward, rotationSpeed * Time.deltaTime);
+        }
+
+        void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (collision.gameObject.CompareTag("Player"))
+            {
+                healthManager.RemoveHealth(20);
+            }
+        }
+    }
+}
---
+++ Player v Player Game/Assets/Scripts/Obsticals/SawObstacle.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using Player_v_Player_Game.Player;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SawObstacle : MonoBehaviour
+{
+    private bool initiateObstacle = false;
+    public float speed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        initiateObstacle = false;
+        StartCoroutine(ObstacleTimer());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (initiateObstacle)
+        {
+            MoveObstacle();
+        }
+    }
+
+    IEnumerator ObstacleTimer()
+    {
+        yield return new WaitForSeconds(15);
+        initiateObstacle = true;
+    }
+
+    private void MoveObstacle()
+    {
+        transform.position += Vector3.down * speed * Time.deltaTime;
+    }
+}

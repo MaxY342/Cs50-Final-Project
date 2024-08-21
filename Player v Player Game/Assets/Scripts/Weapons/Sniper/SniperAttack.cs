6057438b9bbac991ce6d64a19ceaf35e0e4d0c5b
Max <max.yang342@gmail.com>
2024-08-21 19:41:44 -0400

    create sniper

---
+++ Player v Player Game/Assets/Scripts/Weapons/Sniper/SniperAttack.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Player_v_Player_Game.Player;
+using Unity.VisualScripting;
+
+namespace Player_v_Player_Game.Weapons.Revolver
+{
+    public class SniperAttack : MonoBehaviour
+    {
+        [SerializeField] private GameObject bulletPreFab;
+        [SerializeField] private Transform firePoint;
+
+        private TrackCursor trackCursor;
+        // Start is called before the first frame update
+        void Start()
+        {
+            trackCursor = GetComponent<TrackCursor>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                Shoot();
+            }
+        }
+
+        public void Shoot()
+        {
+            float rotation = trackCursor.GetCursorAngleFromObj(firePoint) - 90;
+            Instantiate(bulletPreFab, firePoint.position, Quaternion.Euler(0, 0, rotation));
+        }
+    }
+}
